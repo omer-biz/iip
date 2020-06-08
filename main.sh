3726fc33b882ac94f46bd6c6ac84563053821b80
Omer Abdulaziz <omerabdi@protonmail.com>
2020-06-08 16:07:57 +0300

    done

--- main.sh
+++ main.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/bash
 
-IP="$(ip addr show $1 2>/dev/null | grep inet | head -1 | cut -d ' ' -f 6 | sed 's/\/24//')" 
+IP="$(ip addr show $1 2>/dev/null | grep inet | head -1 | cut -d ' ' -f 6 | sed 's/\/[0-9][0-9]//')" 
 
-if [ IP != '' ] 
+
+if [ $IP ] 
 then
     echo "$1: $IP"
+else
+    echo ""
 fi
