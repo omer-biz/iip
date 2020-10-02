12921f32efda89736fc672b0c0ca3444c78fae3d
Omer Abdulaziz <omerabdi@protonmail.com>
2020-10-02 23:15:10 +0300

    Update main.sh
    less ifs

--- main.sh
+++ main.sh
@@ -2,10 +2,4 @@
 
 IP="$(ip addr show $1 2>/dev/null | grep inet | head -1 | cut -d ' ' -f 6 | sed 's/\/[0-9][0-9]//')" 
 
-
-if [ $IP ] 
-then
-    echo "$1: $IP"
-else
-    echo ""
-fi
+[ $IP ] && echo "$1: $IP"
